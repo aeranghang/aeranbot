d4700c4f4c1aefd5ccaa6bef02f6a5c47b7a78aa
aeranghang <aeranghang@gmail.com>
2018-08-04 11:57:41 +0900

    테스트 추가, 선택지 중 하나 고르는 답변 추가

--- plugins/hello.py
+++ plugins/hello.py
@@ -1,35 +1,8 @@
 import random
-import re
 
 from rtmbot.core import Plugin, Job
 
-
-patterns = {
-    r'(파이[썬선손쏜]|[Pp]ython)': [
-        '파이썬요? 사랑입니다.',
-    ],
-    r'주사위': [
-        lambda m: '주사위를 던졌습니다 => ' + str(random.randint(1, 6)),
-    ],
-    r'동전': [
-        lambda m: '동전을 던졌습니다 => ' + random.choice(['앞면', '뒷면']),
-    ],
-}
-
-
-random_texts = [
-    '과제하기 좋은 날씨입니다.',
-    '수업 피드백을 꼭 남겨주세요.',
-    '맥주는 몽크 IPA',
-    '웅앵웅 쵸키포키',
-    '지속하능하게 갈아넣으세요.',
-    '질문을 많이 해주세요. 조금이라도 궁금하면 그냥 넘어가지 마세요.',
-    '컴퓨터는 맥, 휴대폰은 아이폰, 시계는 애플와치입니다. 물론 에어팟도 사야해요.',
-    '파이썬은 *사랑*입니다.',
-    '한번에 하기 어려우면 더 작은 조각으로 나눠서 해보세요. 중간중간 더 자주 확인하며 진행하세요.',
-    '회고는 하셨나요?',
-    '훌륭해요 :)',
-]
+from canned_reply import reply_to_pattern, random_texts
 
 
 class InitiateChatJob(Job):
@@ -46,17 +19,14 @@ class HelloPlugin(Plugin):
         pass
 
     def process_message(self, data):
-        for p, replies in patterns.items():
-            if re.match(p, data['text']):
-                reply = random.choice(replies)
-                if type(reply) == str:
-                    self.outputs.append([data['channel'], reply])
-                else:
-                    self.outputs.append([data['channel'], reply(data['text'])])
-                return
-
-        if "<@UC3L7FQ7Q>" in data['text']:
+        reply = reply_to_pattern(data['text'])
+        if reply:
+            self.outputs.append([data['channel'], reply])
+            return
+        elif "<@UC3L7FQ7Q>" in data['text']:
             self.outputs.append([
                 data['channel'],
                 random.choice(random_texts)
             ])
+        else:
+            pass
---
+++ tests/test_replies.py
@@ -0,0 +1,26 @@
+from canned_reply import reply_to_pattern as r
+
+
+def test_no_match():
+    assert r('아무말') is None
+
+
+def test_simple_match():
+    assert r('파이썬') == '파이썬요? 사랑입니다.'
+
+
+def test_coin_toss():
+    expected = {'동전을 던졌습니다 => 앞면', '동전을 던졌습니다 => 뒷면'}
+    actual = {r('동전') for _ in range(100)}
+    assert actual == expected
+
+
+def test_choice():
+    expected = {'아이언맨', '슈퍼맨'}
+
+    actual = {r('아이언맨이랑 슈퍼맨 중 누구?') for _ in range(100)}
+    assert actual == expected
+    actual = {r('아이언맨과 슈퍼맨이 싸우면 누가 이겨?') for _ in range(100)}
+    assert actual == expected
+    actual = {r('아이언맨하고 슈퍼맨하고 싸우면 누가 이겨?') for _ in range(100)}
+    assert actual == expected
