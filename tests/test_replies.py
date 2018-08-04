13712955883a644a2cbd67850197f9985f184f03
aeranghang <aeranghang@gmail.com>
2018-08-04 12:24:20 +0900

    대화 추가

--- tests/test_replies.py
+++ tests/test_replies.py
@@ -10,17 +10,24 @@ def test_simple_match():
 
 
 def test_coin_toss():
-    expected = {'동전을 던졌습니다 => 앞면', '동전을 던졌습니다 => 뒷면'}
+    expected = {
+        '동전을 던졌습니다 => 앞면',
+        '동전을 던졌습니다 => 뒷면',
+        '동전을 던졌습니다 => 옆면!?',
+        '동전을 던졌습니다 => 사라졌다!',
+    }
     actual = {r('동전') for _ in range(100)}
     assert actual == expected
 
 
 def test_choice():
-    expected = {'아이언맨', '슈퍼맨'}
+    expected = {'아이언맨', '슈퍼맨', '글쎄?', '정말 이 중에서 골라야 하나요?'}
 
     actual = {r('아이언맨이랑 슈퍼맨 중 누구?') for _ in range(100)}
     assert actual == expected
     actual = {r('아이언맨과 슈퍼맨이 싸우면 누가 이겨?') for _ in range(100)}
     assert actual == expected
     actual = {r('아이언맨하고 슈퍼맨하고 싸우면 누가 이겨?') for _ in range(100)}
+    assert actual == expected
+    actual = {r('아이언맨, 슈퍼맨?') for _ in range(100)}
     assert actual == expected
